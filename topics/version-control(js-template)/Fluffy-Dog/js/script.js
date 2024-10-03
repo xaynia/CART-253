fc144053b470e80683fb508911235e533a236f8d
xaynia <158102890+xaynia@users.noreply.github.com>
2024-10-03 19:22:58 -0400

    Fluffy Dog in Nature
    fluffy dog with googly eyes meets nature
    
    added backdrop:
    - moving clouds
    - growing trees
    - moon
    - shooting stars

--- topics/version-control(js-template)/Fluffy-Dog/js/script.js
+++ topics/version-control(js-template)/Fluffy-Dog/js/script.js
@@ -5,6 +5,13 @@
  * Here is a fluffy dog
  */
 
+// Custom variable for x coordinate of clouds - so they move!
+let cloudOneX = 50;
+
+// Custom variable for moving shooting stars
+let lineXone = 0;
+let lineYone = 0;
+
 /**
  * Creates the canvas
  */
@@ -16,20 +23,103 @@ function setup() {
 }
 
 /**
- * Draws the dog
+ * Draw background
  */
 function draw() {
-    // Sky blue background
-    background(135, 206, 235);
+    // Midnight blue background
+    background(25, 25, 112);
+
+    // Moon (cream)
+    fill(255, 253, 208);
+    circle(350, 50, 100);
+    // Overlap with midnight blue sky color to create crescent moon
+    stroke(25, 25, 112);
+    fill(25, 25, 112);
+    circle(320, 50, 100);
+
+    // Grey mountains
+    stroke(80);
+    fill(80);
+    triangle(-40, 300, 75, 100, 250, 300);
+    triangle(100, 300, 300, 100, 500, 300);
+
+    // Grass (hunter green)
+    stroke(53, 94, 59);
+    fill(53, 94, 59);
+    rect(0, 300, 400, 100);
+
+    // Clouds (white)
+    stroke(255);
+    fill(255);
+    // Cloud 1: left cloud
+    ellipse(cloudOneX, 50, 80, 40);
+    // More clouds!!!
+    ellipse(cloudOneX - 5, 40, 60, 40);
+
+    ellipse(cloudOneX - 30, 90, 50, 10);
+    ellipse(cloudOneX - 40, 100, 70, 20);
+    ellipse(cloudOneX - 50, 110, 50, 20);
+
+    ellipse(cloudOneX + 120, 80, 40, 10);
+    ellipse(cloudOneX + 140, 80, 50, 14);
+
+    // big cloud (right)
+    ellipse(cloudOneX + 300, 30, 130, 19);
+    ellipse(cloudOneX + 320, 20, 125, 30);
+    ellipse(cloudOneX + 280, 30, 130, 25);
+    ellipse(cloudOneX + 330, 20, 135, 20);
+
+    ellipse(cloudOneX - 200, 50, 90, 60);
+    ellipse(cloudOneX - 220, 50, 110, 65);
+    ellipse(cloudOneX - 280, 50, 100, 62);
+
+
+    // This makes clouds move!!! Woot
+    // Set the x coordinate to the frame count
+    // Resets at the left edge
+    cloudOneX = frameCount % width
+    // set shooting star to random location
+    lineXone = random(0, width);
+    lineYone = random(0, height / 2);
+
+    // Trees
+
+    // Different frame rate for trees and shooting stars 
+    frameRate(15); //set frame rate to 15
+
+    // Tree 1
+    // trunk(dark brown)
+    stroke(92, 64, 51);
+    fill(92, 64, 51);
+    rect(40, 270, 15, 50);
+    // leaves (forest green)
+    stroke(34, 139, 34);
+    fill(34, 139, 34);
+    // trees grow vertically via frame count
+    triangle(25, 270, 45, 240 - frameCount % 2900, 70, 270);
+
+    // Tree 2
+    // trunk(dark brown)
+    stroke(92, 64, 51);
+    fill(92, 64, 51);
+    rect(340, 330, 15, 50);
+    // leaves (forest green)
+    stroke(34, 139, 34);
+    fill(34, 139, 34);
+    triangle(325, 330, 345, 240 - frameCount % 290, 370, 330);
+
+    //shooting star
+    stroke("yellow");
+    line(lineXone, lineYone, lineXone + 30, lineYone - 30);
+
 
     // No stroke everywhere!
     noStroke();
-
     drawDog();
 }
 
 /**
- * Draws the dog using functions
+ * Draws the dog
  */
 function drawDog() {
     drawHead();
